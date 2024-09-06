4c0fc389289a3170e223dd51e62182a76ff1dafb
Paulo Sanchez <paulo.sanchezhernandez@alum.uca.es>
2024-09-06 14:51:10 +0200

    Inicio Proyecto Aprender Vue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1 class="title">Hola a my project</h1> 
+</template>
+
+<style scoped>
+  .title {
+    background-color: black;
+    color: white;
+  }
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+<template>
+    <ul>
+        <li>
+            <a href="#">Home</a>
+        </li>
+        <li>
+            <a href="#">Galeria</a>
+        </li>
+        <li>
+            <a href="#">Contacto</a>
+        </li>
+    </ul>
+</template>
+<style></style>
